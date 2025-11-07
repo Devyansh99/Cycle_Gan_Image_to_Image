e42e1d407c53e887847018c9bac3fe733d2890d3
Devyansh99 <devyanshag24@gmail.com>
2025-11-08 00:12:24 +0530

    Fixed issues

--- options/base_options.py
+++ options/base_options.py
@@ -122,6 +122,19 @@ class BaseOptions:
             suffix = ("_" + opt.suffix.format(**vars(opt))) if opt.suffix != "" else ""
             opt.name = opt.name + suffix
 
+        # set gpu ids
+        str_ids = getattr(opt, 'gpu_ids', '0').split(',') if hasattr(opt, 'gpu_ids') else ['0']
+        opt.gpu_ids = []
+        for str_id in str_ids:
+            id = int(str_id)
+            if id >= 0:
+                opt.gpu_ids.append(id)
+        if len(opt.gpu_ids) > 0:
+            torch.cuda.set_device(opt.gpu_ids[0])
+        
+        # set device
+        opt.device = torch.device('cuda:{}'.format(opt.gpu_ids[0])) if opt.gpu_ids else torch.device('cpu')
+
         self.print_options(opt)
         self.opt = opt
         return self.opt
